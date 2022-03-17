1df1f1a571ffb00a58afec200372ea4df4e1f8c5
JoaoFreire1 <joao.freire.art@gmail.com>
2022-03-17 15:11:43 +0000

    Fourth Commit. TiposVariaveis |int| done. Currently at step 4

---
+++ TiposVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TiposVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int um = 1;
+            int dois = 2;
+            int tres = 3;
+            int quatro = 4;
+            int cinco = 5;
+
+            Console.WriteLine(um);
+            Console.WriteLine(dois);
+            Console.WriteLine(tres);
+            Console.WriteLine(quatro);
+            Console.WriteLine(cinco);
+
+
+        }
+    }
+}
