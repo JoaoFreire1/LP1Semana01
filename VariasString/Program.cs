9c573f598ab997da6231dc4bfebb9545e429a8dd
JoaoFreire1 <joao.freire.art@gmail.com>
2022-03-17 16:00:07 +0000

    Nineth Commit. Halfway done with exercise 5!

--- VariasString/Program.cs
+++ VariasString/Program.cs
@@ -8,12 +8,21 @@ namespace VariasString
         {
             string normal = "Isto é uma string normal";
             string verbatim = "C:\\UsersNome_GenericoDocuments";
-            string unicode = "Palavras\u004F\u004C\u0041";
+            string unicode = "Palavras \u004F\u004C\u0041";
 
             Console.WriteLine (normal);
             Console.WriteLine (verbatim);
             Console.WriteLine (unicode);
-            
+
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            string concatenacao = "Isto " + "são " + 2 + " strings";
+            string interpolacao = $"{a} mais {b} é igual a {c}";
+
+            Console.WriteLine (concatenacao);
+            Console.WriteLine (interpolacao);
         }
     }
 }
