e3b19b5804a0ae82f92145dbb81faee99ef80378
JoaoFreire1 <joao.freire.art@gmail.com>
2022-03-17 15:48:52 +0000

    Eight Commit. Finished Exercise 4!

---
+++ VariasString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariasString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string normal = "Isto é uma string normal";
+            string verbatim = "C:\\UsersNome_GenericoDocuments";
+            string unicode = "Palavras\u004F\u004C\u0041";
+
+            Console.WriteLine (normal);
+            Console.WriteLine (verbatim);
+            Console.WriteLine (unicode);
+            
+        }
+    }
+}
