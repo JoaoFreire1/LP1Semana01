127891cc5b05479783d5deb99f28d963864035f9
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-02 23:43:54 +0000

    Exercise 04

---
+++ VariasString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace VariasString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string a = "Primeira String";
+            Console.WriteLine(a);
+            string b = "Segunda String";
+            Console.WriteLine(b);
+            string v1 = @"/String Verbatim\";
+            Console.WriteLine(v1);
+            string u1 = "String com Unicode \u0F23";
+            Console.WriteLine(u1);
+            string u2 = "String com Unicode \u2682";
+            Console.WriteLine(u2);
+        }
+    }
+}
