a1b54c8b4a3882edc9aca166c3d91f97a4ae9b6e
JoaoFreire1 <92170119+JoaoFreire1@users.noreply.github.com>
2023-03-03 13:28:04 +0000

    Exercise 07, halfway

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArgLongStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                for (int i = 0; i > args[i].Length; i ++)
+	            {
+	                int x = args[i].Length;
+                    if (x > 4):
+                        {
+                            Console.WriteLine(args[i]);
+                        }
+
+	            }
+            }
+        }
+    }
+}
