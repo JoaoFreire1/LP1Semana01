aa30253bcc914d5cf9ad6a8253014bc03cc74805
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-20 21:24:43 +0000

    Int Variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 1;
+            long aa = 11;
+            short aaa = 111;
+            Console.WriteLine($"{a}, {aa}, {aaa}");
+        }
+    }
+}
