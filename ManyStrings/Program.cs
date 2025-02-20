df784f7078a8cb36c0a8ccb5953db6efab857155
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-20 22:03:57 +0000

    Exercise 5

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,6 +10,11 @@ namespace ManyStrings
             string bob = "Eu sou o bob";
             string string2 = "AHHHHH \u0032"
             Console.WriteLine($"{string1}, {bob}, {string2}");
+
+            string string3 = "Im in Pain" + 5;
+            string string4 = $"SHBD {bob}";
+            string string5 = $"{bob} is bob-ing" + 3;
+            Console.WriteLine($"{string3}, {string4}, {string5}");
         }
     }
 }
