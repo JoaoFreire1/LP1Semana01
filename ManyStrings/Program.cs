90c1f4ea24a9c6359c2169621c190be13a4ca11f
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-20 21:59:37 +0000

    ManyStrings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string string1 = "Eu nao tenho Unicode";
+            string bob = "Eu sou o bob";
+            string string2 = "AHHHHH \u0032"
+            Console.WriteLine($"{string1}, {bob}, {string2}");
+        }
+    }
+}
