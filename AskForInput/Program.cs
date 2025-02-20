434824b02907f4227e2db0f3f70f93687abeea05
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-20 22:59:02 +0000

    Addition

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -11,8 +11,10 @@ namespace AskForInput
             int i = int.Parse(input);
             Console.WriteLine("Por favor escreva um numero real");
             string input2 = Console.ReadLine();
-            float f = float.Parse(input2)
-            
+            float f = float.Parse(input2);
+            int i2 = Convert.ToInt32(f);
+            int i3 = i + i2;
+            Console.WriteLine($"A soma de {i} com {i2} é {i3}");
         }
     }
 }
