88cd0403af5d260227192220d26d40231dada1c1
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-20 21:47:07 +0000

    real variables

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+ using System.Text;
 
 namespace VariousTypes
 {
@@ -6,10 +7,22 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8; 
+
             int a = 1;
             long aa = 11;
             short aaa = 111;
             Console.WriteLine($"{a}, {aa}, {aaa}");
+
+            char b = '\u0030';
+            char bb = '\u0031';
+            char bbb = '\u0032';
+            Console.WriteLine($"{b}, {bb}, {bbb}");
+
+            float c = 3,14f;
+            double cc = 3,15;
+            decimal ccc = 3,16m;
+            Console.WriteLine($"{c}; {cc}; {ccc}");
         }
     }
 }
