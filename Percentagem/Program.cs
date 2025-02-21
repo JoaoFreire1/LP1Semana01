a082942519ee313b20c7ed05459c2eaca870e73c
JoaoFreire1 <joao.freire.art@gmail.com>
2025-02-21 14:11:04 +0000

    Inputs are float variables

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -11,6 +11,14 @@ namespace Percentagem
             string input2 = Console.ReadLine();
             string input3 = Console.ReadLine();
             string input4 = Console.ReadLine();
+            float nreal1 = float.Parse(input1);
+            float nreal2 = float.Parse(input2);
+            float nreal3 = float.Parse(input3);
+            float nreal4 = float.Parse(input4);
+            Console.WriteLine(nreal1);
+            Console.WriteLine(nreal2);
+            Console.WriteLine(nreal3);
+            Console.WriteLine(nreal4);
         }
     }
 }
